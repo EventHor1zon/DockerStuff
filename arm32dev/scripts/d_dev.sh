5c50399ba8b74bc237bcc226308417082dadf939
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2023-08-19 22:49:56 +0100

    remove nvim config mounting, use dotfiles repo instead

--- arm32dev/scripts/d_dev.sh
+++ arm32dev/scripts/d_dev.sh
@@ -16,13 +16,13 @@ DOCKER_COMMAND="docker run -it \
                 -u $USERUID:$USERGID \
                 --mount type=bind,src="$(pwd)",target="$SRC_DIRECTORY
 
-if [[ -e ~/.config/nvim/init.vim || -e ~/.config/nvim/init.lua  ]]; then
-    echo "[+] Found neovim config, mounting..."
-    DOCKER_COMMAND=$DOCKER_COMMAND" --mount type=bind,src=/home/"$USER"/.config/nvim/,target=/home/dev/.config/nvim/"
-elif [[ -z ~/.config/vim ]]; then
-    echo "[+] Found vim config, mounting..."
-    DOCKER_COMMAND=$DOCKER_COMMAND" --mount type=bind,src=/home/"$USER"/.config/vim/,target=/home/dev/.config/vim/"
-fi
+#if [[ -e ~/.config/nvim/init.vim || -e ~/.config/nvim/init.lua  ]]; then
+#    echo "[+] Found neovim config, mounting..."
+ #   DOCKER_COMMAND=$DOCKER_COMMAND" --mount type=bind,src=/home/"$USER"/.config/nvim/,target=/home/dev/.config/nvim/"
+#elif [[ -z ~/.config/vim ]]; then
+#    echo "[+] Found vim config, mounting..."
+#    DOCKER_COMMAND=$DOCKER_COMMAND" --mount type=bind,src=/home/"$USER"/.config/vim/,target=/home/dev/.config/vim/"
+#fi
 
 # if [[ -e ~/.tmux.conf ]]; then 
 #     echo "[+] Found Tmux config, mounting..."
